cc8d9d773ba2b697adce880267f9b7451033a5be
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-10-29 04:40:30 +0530

    [abhishek] add: insert data in employee_payroll tabel

--- Employee_payroll.sql
+++ Employee_payroll.sql
@@ -11,3 +11,9 @@ salary double not null,
 start date not null,
 primary key(id)
 );
+
+#uc3
+insert into  employee_payroll (name ,salary,start) values
+('Bill' ,12345,'2018-01-03'),
+('Terisa',20000,'2018-11-13'),
+('charlie',3940309,'2018-12-21');
