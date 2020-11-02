02f2d4f69f5654a3caf973d13856a989b6256b62
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-11-02 19:40:06 +0530

    [abhishek] add: implement the ER Diagram

--- Employee_payroll.sql
+++ Employee_payroll.sql
@@ -52,3 +52,66 @@ alter table employee_payroll add net_pay varchar(200)  not null after income_tax
 #UC10
 insert into  employee_payroll (name ,salary,start,department,gender,phone_number,deductions,basic_pay,taxable_pay,income_tax,net_pay) values
 ('Terisa',20000,'2018-11-13','Sales','F','0','0','0','0','0','0'),('Terisa',20000,'2018-11-13','Department','F','0','0','0','0','0','0');
+
+#uc11
+CREATE TABLE company						
+(
+ company_id 	INT primary key, 				
+ company_name 	VARCHAR(30) NOT NULL
+);
+
+
+create table employee(
+emp_id INT NOT NULL,
+name VARCHAR(150) NOT NULL,
+address VARCHAR(150) NOT NULL,
+gender VARCHAR(150) NOT NULL,
+start DATE NOT NULL,
+PRIMARY KEY (emp_id),
+company_id int,
+foreign key(company_id) REFERENCES company(company_id)
+);
+
+
+select * from employee;
+create table  Emplyee_Department
+(emp_id int,
+dept_id int ,
+primary key(emp_id,dept_id),
+foreign key(emp_id) references employee(emp_id),
+foreign key(dept_id) references department(dept_id));
+
+
+create table payroll(
+basic_pay double not null,
+taxable_pay double not null,
+income_tax double not null,
+deduction double not null,
+emp_id int,
+foreign key(emp_id) references employee(emp_id));
+create table department(
+dept_id int primary key,
+dept_name varchar(200),
+dept_hod varchar(200)
+);
+
+
+INSERT INTO company VALUES 	
+(1,'Capgemini');
+
+
+insert into  employee (emp_id,name ,address,gender,start,company_id) values
+(1,'Bill' ,'jsr','M','2018-01-03',1),
+(2,'Terisa','ranchi','F','2018-11-13',1),
+(3,'charlie','delhi','M','2018-12-21',1);
+
+INSERT INTO payroll(basic_pay,taxable_pay,income_tax,deduction,emp_id) VALUES 					
+(52000.00,2000.00,45000.00,5000.00,1);
+
+INSERT INTO department VALUES  					 
+(100, 'Sales','abc'),
+(200, 'Marketing','xyz');
+
+INSERT INTO employee_department VALUES  				
+(1,100),
+(1,200);
